b28e8b5eefa34dcd0b83757aee397580ae1368d6
Rohan Lande <rohan.lande@caizin.com>
2026-02-18 11:04:01 +0530

    feat: add OpenAI API request and response DTOs

---
+++ src/main/java/com/caizin/recruitment/dto/OpenAiRequestDto.java
@@ -0,0 +1,109 @@
+package com.caizin.recruitment.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OpenAiRequestDto {
+    private String model;
+    private List<Message> messages;
+    private Double temperature;
+
+    @JsonProperty("response_format")
+    private ResponseFormat responseFormat;
+
+    public OpenAiRequestDto() {
+    }
+
+    public OpenAiRequestDto(String model, List<Message> messages, Double temperature, ResponseFormat responseFormat) {
+        this.model = model;
+        this.messages = messages;
+        this.temperature = temperature;
+        this.responseFormat = responseFormat;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    public ResponseFormat getResponseFormat() {
+        return responseFormat;
+    }
+
+    public void setResponseFormat(ResponseFormat responseFormat) {
+        this.responseFormat = responseFormat;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Message {
+        private String role;
+        private String content;
+
+        public Message() {
+        }
+
+        public Message(String role, String content) {
+            this.role = role;
+            this.content = content;
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class ResponseFormat {
+        private String type;
+
+        public ResponseFormat() {
+        }
+
+        public ResponseFormat(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+    }
+}
+
